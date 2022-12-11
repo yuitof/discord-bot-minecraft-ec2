141d3e2bfced47d670afdfe2ec1377a22a57ce1a
BUIBUI <yuito.fujiwara.0705@gmail.com>
2022-12-11 13:42:43 +0900

    Instances can now be launched.

--- test.py
+++ test.py
@@ -1,14 +1,18 @@
 import discord
 import os
+import boto3
 from dotenv import load_dotenv
 
 load_dotenv()
 
+INSTANCE_ID = os.getenv("INSTANCE_ID")
+TOKEN = os.getenv('TOKEN')
+
 Intents = discord.Intents.all()
 client = discord.Client(intents = Intents)
 
-TOKEN = os.getenv('TOKEN')
-print(TOKEN)
+ec2 = boto3.resource('ec2')
+instance = ec2.Instance(INSTANCE_ID)
 
 @client.event
 async def on_ready():
@@ -18,7 +22,22 @@ async def on_ready():
 async def on_message(message):
     if message.author.bot:
         return
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
+    if message.content == '/start':
+        print("Instance is starting")
+        await message.channel.send('Instance is starting')
+        instance.start()
+        instance.wait_until_running()
+        print("Instance started")
+        await message.channel.send('Instance is running')
+        ip = instance.public_ip_address
+        print(f"IP_ADDRESS: {ip}")
+
+    if message.content == '/stop':
+        print("instance is stopping")
+        await message.channel.send('instance is stopping')
+        instance.stop()
+        instance.wait_until_stopped()
+        print("instance is stopped")
+        await message.channel.send('instance is stopped')
 
 client.run(TOKEN)
